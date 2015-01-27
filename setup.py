abb7c8578fcd131c99dbb9ca4c351e4de4cb7945
Thomas Levine <_@thomaslevine.com>
2015-01-27 11:00:23 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='io-whois',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Download WHOIS data for popular .io domains',
+      url='https://small.dada.pink/io-whois',
+      install_requires = ['vlermv','requests','lxml'],
+      py_modules=['whois'],
+      entry_points={'console_scripts': ['io-whois = whois:main']},
+      version='0.0.1',
+      license='AGPL',
+)
