6693225397f0ef977baf113a2fec18bbe674ecff
Thomas Levine <_@thomaslevine.com>
2015-01-27 10:30:28 +0000

    csv

--- whois.py
+++ whois.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import csv
 from functools import partial
 import datetime
 
@@ -8,14 +9,21 @@ import lxml.html
 
 url = 'http://nic.io/cgi-bin/whois'
 
-@vlermv.cache('~/.io-whois')
+@vlermv.cache('~/.io-whois/other')
+def get_other(url):
+    return requests.get(url)
+
+@vlermv.cache('~/.io-whois/whois')
 def get(domain):
+    'Get a WHOIS record.'
     return requests.get(url, params = {'query': domain})
 
 def _query_html(html, key):
+    'Query the body of a WHOIS record.'
     return str(html.xpath('//td[text() = "%s"]/following-sibling::td/text()' % key)[0])
 
 def parse(response):
+    'Parse a whois response.'
     html = lxml.html.fromstring(response.text)
     query = partial(_query_html, html)
     date = datetime.datetime.strptime(query('First Registered :'), '%Y-%m-%d').date()
@@ -23,3 +31,29 @@ def parse(response):
         'domain-name': query('Domain Name :'),
         'first-registered': date,
     }
+
+def most_popular():
+    'List the most popular .io domains, based on someone\'s idea of popularity.'
+    response = get_other('http://hack.ly/articles/the-most-popular-dot-io-domains/')
+    html = lxml.html.fromstring(response.text)
+    return map(str, html.xpath('//div[@class="entry-content"]/descendant::a/text()'))
+
+def main():
+    import sys
+    writer = csv.DictWriter(sys.stdout, ('domain-name', 'first-registered'))
+    for domain in most_popular():
+        response = get(domain)
+        if not response.ok:
+            sys.stderr.write('Error downloading "%s"\n' % domain)
+            sys.exit(1)
+
+        try:
+            data = parse(response)
+        except Exception as e:
+            sys.stderr.write('Error parsing the "%s" record:\n%s\n' % (domain, e))
+            sys.exit(2)
+
+        writer.writerow(data)
+
+if __name__ == '__main__':
+    main()
